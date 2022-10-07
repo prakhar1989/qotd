9bceeb2515ddbaa871a36264db3308ea77c39cae
Prakhar Srivastav <prakhar1989@gmail.com>
2022-10-06 21:43:33 -0700

    add firebase integration and courier integration

--- quote.py
+++ quote.py
@@ -1,14 +1,25 @@
-import re
 from PIL import Image, ImageFont, ImageDraw 
 from random import choice
+from datetime import datetime
 import shutil
 import json
+import os
 import requests
 import textwrap
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
 
 image_backdrops = ['landscape', 'nature', 'stars', 'mountains']
 canned_seeds = []
 title_font = ImageFont.truetype('lato.ttf', 48)
+
+cred = credentials.Certificate('serviceaccount.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': '{}.appspot.com'.format(os.environ["FIREBASE_BUCKET"])
+})
+bucket = storage.bucket()
+
 
 # read seeds from file
 with open('seeds.txt') as f:
@@ -23,6 +34,7 @@ def download_img():
 
 
 def generate_img(text):
+    print("Generating image")
     img = Image.open("img.png")
     width, height = img.size
     image_editable = ImageDraw.Draw(img)
@@ -37,12 +49,45 @@ def generate_img(text):
     img.save("result.jpg")
     print("generated result.jpg")
 
+def upload_img(filename):
+    print("Uploading image")
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)
+    blob.make_public()
+    return blob.public_url
 
 def get_quote():
+    print("Generating quote")
     seed = choice(canned_seeds)
     resp = requests.post('http://127.0.0.1:8000/generate', data=json.dumps({"text": seed}))
     return resp.json()["text"]
 
+def send_courier(image_url):
+    print("Sending courier")
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(os.environ['COURIER_AUTH_TOKEN'])
+    }
+    message={
+        "to": { "email": os.environ["COURIER_RECIPIENT"] },
+        "data": {
+            "date": datetime.today().strftime("%B %d, %Y"),
+            "img": image_url
+        },
+        "routing": {
+            "method": "single",
+            "channels": [
+                "email"
+            ]
+        },
+        "template": os.environ["COURIER_TEMPLATE"]
+    }
+    resp = requests.post("https://api.courier.com/send", json={"message": message}, headers=headers)
+    print(resp.json())
+
 quote = get_quote()
 download_img()
 generate_img(quote)
+img_url = upload_img("result.jpg")
+send_courier(img_url)
